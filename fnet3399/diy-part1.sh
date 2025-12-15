f18e7239ecb1e89cf4ffce11b57ead40ca8d3298
nrxboat <nrxboat@outlook.com>
2025-12-15 18:03:57 +0800

    Modify author and add new packages to script
    Updated author name and added luci-app-tailscale and luci-theme-aurora.

--- fnet3399/diy-part1.sh
+++ fnet3399/diy-part1.sh
@@ -12,6 +12,13 @@ date_version=$(date +"%Y%m")
 echo $date_version > version
 
 # 为iStoreOS固件版本加上编译作者
-author="Lemon1151"
+author="Lemon1"
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version} by ${author}'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}\"/g" package/base-files/files/usr/lib/os-release
+
+# Add luci-app-tailscale and luci-theme-aurora
+./scripts/feeds update -a
+./scripts/feeds install -a
+sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
